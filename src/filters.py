eef608f608b4c1cf3d0dba3346e46174649f6c78
Lucas Alexandre Soares <lucassoares1793@gmail.com>
2017-06-30 08:10:11 -0300

    menu done

--- src/filters.py
+++ src/filters.py
@@ -3,89 +3,36 @@ from PIL import Image, ImageOps
 from PIL import ImageFilter
 import sys
 
+# Filters
 def applyFilter(image, operation):
-	options = {'blur': ImageFilter.BLUR,
-			  'contour': ImageFilter.CONTOUR,
-			  'detail': ImageFilter.DETAIL,
-			  'edge_enhance': ImageFilter.EDGE_ENHANCE,
-			  'edge_enhance_more': ImageFilter.EDGE_ENHANCE_MORE,
-			  'emboss': ImageFilter.EMBOSS,
-			  'find_edges': ImageFilter.FIND_EDGES,
-			  'smooth': ImageFilter.SMOOTH,
-			  'smooth_more': ImageFilter.SMOOTH_MORE,
-			  'sharpen': ImageFilter.SHARPEN
-			  }
+	return image.filter(operation)
 
-	if operation not in options:
-		raise ValueError("Unknown filter type.")
+def autocontrast(img):
+	return ImageOps.autocontrast(img, cutoff=0, ignore=None)
 
-	return image.filter(options[operation])
-
-image = Image.open(str(sys.argv[1])).convert("RGB") 
-
-#filtered = applyFilter(image, str(sys.argv[2]))
-#filtered.show()
-
-#black = (255, 0, 0)
-#white = (0, 255, 0)
-
-bits = 3
-opt = sys.argv[2]
-# = input(); 
-
-if(opt == 'autocontrast'):
-	image2 = ImageOps.autocontrast(image, cutoff=0, ignore=None)
-	image2.show()
-
-'''if(opt == 'colorize'):
-	image2 = ImageOps.colorize(image, black, white) 
-	image2.show()'''
-
-if(opt == 'crop'):
-	image2 = ImageOps.crop(image, border=0)
-	image2.show()
-
-'''if(opt == 'deform'):
-	image2 = ImageOps.deform(image, deformer, resample=2)
-	image2.show()'''
-
-if(opt == 'equalize'): 
-	image2 = ImageOps.equalize(image, mask=None)
-	image2.show()
+def equalize(img):
+	return ImageOps.equalize(img, mask=None)
 	
-if(opt == 'expand'): 
-	image2 = ImageOps.expand(image, border=0, fill=0)
-	image2.show()
+def flip(img):
+	return ImageOps.flip(img)
 	
-'''if(opt == 'fit'): 
-	image2 = ImageOps.fit(image, size, method=0, bleed=0.0, centering=(0.5, 0.5))
-	image2.show()'''
+def grayscale(img):
+	return ImageOps.grayscale(img)
 	
-if(opt == 'flip'):
-	print ("testing flip") 
-	image2 = ImageOps.flip(image)
-	image2.show()
+def invert(img):
+	return ImageOps.invert(img)
 	
-if(opt == 'grayscale'): 
-	image2 = ImageOps.grayscale(image)
-	image2.show()
+def mirror(img):
+	return ImageOps.mirror(img)
 	
-if(opt == 'invert'): 
-	image2 = ImageOps.invert(image)
-	image2.show()
+def posterize(img):
+	return ImageOps.posterize(img, bits=3)
 	
-if(opt == 'mirror'):
-	image2 = ImageOps.mirror(image)
-	image2.show()
-	
-if(opt == 'posterize'): 
-	image2 = ImageOps.posterize(image, bits)
-	image2.show()
-	
-if(opt == 'solarize'): 
-	image2 = ImageOps.solarize(image, threshold=128)
-	image2.show()
+def solarize(img):
+	return ImageOps.solarize(img, threshold=128)
 
+
+# Distortion functions
 def quad_as_rect(quad):
     if quad[0] != quad[2]: return False
     if quad[1] != quad[7]: return False
@@ -93,70 +40,86 @@ def quad_as_rect(quad):
     if quad[3] != quad[5]: return False
     return True
 
-def quad_to_rect(quad):
+def quad2rect(quad):
     assert(len(quad) == 8)
     assert(quad_as_rect(quad))
     return (quad[0], quad[1], quad[4], quad[3])
 
-def rect_to_quad(rect):
+def rect2quad(rect):
     assert(len(rect) == 4)
     return (rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1])
 
-def shape_to_rect(shape):
+def createRect(shape):
     assert(len(shape) == 2)
     return (0, 0, shape[0], shape[1])
 
-def griddify(rect, w_div, h_div):
+def griddify(rect, xRes, yRes):
+    
+    # Get width and height from original image
     w = rect[2] - rect[0]
     h = rect[3] - rect[1]
-    x_step = w / float(w_div)
-    y_step = h / float(h_div)
+    
+    # Calculate step based on distortion resolution
+    xStep = w / float(xRes)
+    yStep = h / float(yRes)
+    
     y = rect[1]
-    grid_vertex_matrix = []
-    for _ in range(h_div + 1):
-        grid_vertex_matrix.append([])
+    vertexMat = []
+    
+    for _ in range(yRes + 1):
+    
+        vertexMat.append([])
         x = rect[0]
-        for _ in range(w_div + 1):
-            grid_vertex_matrix[-1].append([int(x), int(y)])
-            x += x_step
-        y += y_step
-    grid = np.array(grid_vertex_matrix)
+
+        for _ in range(xRes + 1):
+            vertexMat[-1].append([int(x), int(y)])
+            x += xStep
+
+        y += yStep
+
+    grid = np.array(vertexMat)
+
     return grid
 
-def distort_grid(org_grid, max_shift):
-    new_grid = np.copy(org_grid)
-    x_min = np.min(new_grid[:, :, 0])
-    y_min = np.min(new_grid[:, :, 1])
-    x_max = np.max(new_grid[:, :, 0])
-    y_max = np.max(new_grid[:, :, 1])
-    new_grid += np.random.randint(- max_shift, max_shift + 1, new_grid.shape)
-    new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
-    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
-    new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
-    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
-    return new_grid
+def distort(org_grid, maxDistortion):
 
-def grid_to_mesh(src_grid, dst_grid):
-    assert(src_grid.shape == dst_grid.shape)
+    newGrid = np.copy(org_grid)
+    
+    minX = np.min(newGrid[:, :, 0])
+    minY = np.min(newGrid[:, :, 1])
+    maxX = np.max(newGrid[:, :, 0])
+    maxY = np.max(newGrid[:, :, 1])
+    
+    # Create a random matrix
+    newGrid += np.random.randint(-maxDistortion, maxDistortion+1, newGrid.shape)
+
+    # Populate it getting maximum and minimum inside original range
+    newGrid[:, :, 0] = np.maximum(minX, newGrid[:, :, 0])
+    newGrid[:, :, 1] = np.maximum(minY, newGrid[:, :, 1])
+    newGrid[:, :, 0] = np.minimum(maxX, newGrid[:, :, 0])
+    newGrid[:, :, 1] = np.minimum(maxY, newGrid[:, :, 1])
+    
+    return newGrid
+
+def gridToMesh(src, dest):
+    
+    assert(src.shape == dest.shape)
     mesh = []
-    for i in range(src_grid.shape[0] - 1):
-        for j in range(src_grid.shape[1] - 1):
-            src_quad = [src_grid[i    , j    , 0], src_grid[i    , j    , 1],
-                        src_grid[i + 1, j    , 0], src_grid[i + 1, j    , 1],
-                        src_grid[i + 1, j + 1, 0], src_grid[i + 1, j + 1, 1],
-                        src_grid[i    , j + 1, 0], src_grid[i    , j + 1, 1]]
-            dst_quad = [dst_grid[i    , j    , 0], dst_grid[i    , j    , 1],
-                        dst_grid[i + 1, j    , 0], dst_grid[i + 1, j    , 1],
-                        dst_grid[i + 1, j + 1, 0], dst_grid[i + 1, j + 1, 1],
-                        dst_grid[i    , j + 1, 0], dst_grid[i    , j + 1, 1]]
-            dst_rect = quad_to_rect(dst_quad)
-            mesh.append([dst_rect, src_quad])
-    return mesh
+    
+    for i in range(src.shape[0] - 1):
+        for j in range(src.shape[1] - 1):
+            
+            srcQuad = [src[i    , j    , 0], src[i    , j    , 1],
+                       src[i + 1, j    , 0], src[i + 1, j    , 1],
+                       src[i + 1, j + 1, 0], src[i + 1, j + 1, 1],
+                       src[i    , j + 1, 0], src[i    , j + 1, 1]]
+            
+            destQuad = [dest[i    , j    , 0], dest[i    , j    , 1],
+                        dest[i + 1, j    , 0], dest[i + 1, j    , 1],
+                        dest[i + 1, j + 1, 0], dest[i + 1, j + 1, 1],
+                        dest[i    , j + 1, 0], dest[i    , j + 1, 1]]
+            
+            dst_rect = quad2rect(destQuad)
+            mesh.append([dst_rect, srcQuad])
 
-#im = Image.open('./old_driver/data/train/c0/img_292.jpg')
-im = image
-dst_grid = griddify(shape_to_rect(im.size), 4, 4)
-src_grid = distort_grid(dst_grid, 50)
-mesh = grid_to_mesh(src_grid, dst_grid)
-im = im.transform(im.size, Image.MESH, mesh)
-im.show()	
+    return mesh
---
+++ src/main.py
@@ -0,0 +1,185 @@
+'''
+Main source file for Facial C
+
+
+'''
+from PIL import Image, ImageOps, ImageFilter
+import filters
+from filters import *
+
+import numpy as np
+import cv2
+import sys
+import os
+
+# Define possible filters in options dict
+options = {'blur': ImageFilter.BLUR,
+		  'contour': ImageFilter.CONTOUR,
+		  'detail': ImageFilter.DETAIL,
+		  'edge_enhance': ImageFilter.EDGE_ENHANCE,
+		  'edge_enhance_more': ImageFilter.EDGE_ENHANCE_MORE,
+		  'emboss': ImageFilter.EMBOSS,
+		  'find_edges': ImageFilter.FIND_EDGES,
+		  'smooth': ImageFilter.SMOOTH,
+		  'smooth_more': ImageFilter.SMOOTH_MORE,
+		  'sharpen': ImageFilter.SHARPEN
+		  }
+options2 = ['autocontrast', 
+		   'equalize', 
+		   'flip', 
+		   'invert', 
+		   'grayscale',
+		   'mirror', 
+		   'posterize', 
+		   'solarize'
+		   ]
+
+# Menu handlers
+def printMenu():
+	print("\n\t1 - Load image")
+	print("\t2 - Filter image")
+	print("\t3 - Distort image")
+	print("\t4 - Remove red eyes image")
+	print("\t5 - Reset")
+	print("\t0 - Exit")
+
+def printFilterMenu():
+	print("\n\tFilters: blur, contour, detail, edge_enhance, emboss, find_edges, smooth, sharpen, autocontrast, equalize, flip, invert, mirror, posterize, solarize")
+
+def filterMenu(img):
+
+	printFilterMenu()
+	operation = input()
+
+	if operation in options:
+
+		try:
+			img2 = applyFilter(img, options[operation])
+
+		except Exception as e:
+			raise
+
+	elif operation in options2:
+
+		try:
+			img2 = getattr(filters, operation)(img)
+
+		except Exception as e:
+			raise
+
+	else:
+		raise Exception("\nUnknown operation")
+
+	return img2
+
+def distortMenu(img):
+	
+	try:
+		xGridResolution = int(input("x Grid Resolution: "))
+		yGridResolution = int(input("y Grid Resolution: "))
+		distortion = int(input("Max Distortion: "))
+
+	except Exception as e:
+		print("\nInvalid parameters.")
+		raise
+
+	dest = griddify(createRect(img.size), xGridResolution, yGridResolution)
+	src = distort(dest, distortion)
+
+	return img.transform(img.size, Image.MESH, gridToMesh(src, dest))
+
+# Helpers
+def pil2cv(img):
+	return np.array(img)[:, :, ::-1] # Convert from RGB to BGR
+
+def cv2pil(img):
+	return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)) # Convert from BGR to RGB
+
+def updateImg(processingImg):
+	cv2.destroyWindow("Modified")
+	cv2.imshow("Modified", processingImg)
+	cv2.waitKey(200)
+
+
+########
+# Main #
+########
+
+# Select initial image
+# selectedImg = Image.open(input("Image file: ")).convert("RGB")
+selectedImg = Image.open("databases/tests/Anzu3.jpg").convert("RGB")
+processingImg = selectedImg.copy()
+
+# Convert from pillow to opencv format and display images
+cv2.imshow("Original", pil2cv(selectedImg))
+cv2.imshow("Modified", pil2cv(processingImg))
+cv2.waitKey(200)
+
+# Loop forever
+while True:
+
+	printMenu()
+
+	# Transform op in invalid command
+	try:
+		op = int(input())
+	except Exception as e:
+		op = -1
+
+	if op == 0:
+		
+		# Cleanup and exit
+		cv2.destroyAllWindows()
+		sys.exit()
+
+	elif op == 1:
+
+		try:
+
+			selectedImg = Image.open(input("Image file: ")).convert("RGB")
+			processingImg = selectedImg.copy()
+			cv2.destroyAllWindows()
+			
+			# Convert from pillow to opencv format and display images
+			tmp = pil2cv(selectedImg)
+			cv2.imshow("Original", tmp)
+			cv2.imshow("Modified", tmp)
+			cv2.waitKey(100)
+
+		except IOError as e:
+			print("\nImage not found.")
+
+	elif op == 2:
+		try:
+			
+			tmp = filterMenu(processingImg)
+			
+			# Preserve processing image in case something bad happens
+			processingImg = tmp
+			updateImg(pil2cv(processingImg))
+
+		except Exception as e:
+			print(e)
+	
+	elif op == 3:
+		try:
+			
+			tmp = distortMenu(processingImg)
+			
+			# Preserve processing image in case something bad happens
+			processingImg = tmp
+			updateImg(pil2cv(processingImg))
+			
+		except Exception as e:
+			print(e)
+
+	elif op == 4:
+		print("TODO")
+
+	elif op == 5:
+		processingImg = selectedImg
+		updateImg(pil2cv(processingImg))
+
+	else:
+		print("\nUnknown operation.")
+
