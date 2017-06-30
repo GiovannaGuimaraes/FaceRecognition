bf2bdc2a37a10bed65fe4a69ffdb2876dd85ecbf
Julia Diniz <judinizf@gmail.com>
2017-06-30 02:44:27 -0300

    batata

--- src/filters.py
+++ src/filters.py
@@ -21,55 +21,66 @@ def applyFilter(image, operation):
 	return image.filter(options[operation])
 
 image = Image.open(str(sys.argv[1])).convert("RGB") 
-filtered = applyFilter(image, str(sys.argv[2]))
-filtered.show()
-
-'''
-image2 = PIL.ImageOps.autocontrast(image, cutoff=0, ignore=None)
+#filtered = applyFilter(image, str(sys.argv[2]))
+#filtered.show()
 
 #black = (255, 0, 0)
 #white = (0, 255, 0)
-#image2 = PIL.ImageOps.colorize(image, black, white)
 
-image2 = PIL.ImageOps.crop(image, border=0)
+bits = 3
+opt = sys.argv[2]
+# = input(); 
 
-#image2 = PIL.ImageOps.deform(image, deformer, resample=2)
+if(opt == 'autocontrast'):
+	image2 = ImageOps.autocontrast(image, cutoff=0, ignore=None)
+	image2.show()
 
-image2 = PIL.ImageOps.equalize(image, mask=None)
+'''if(opt == 'colorize'):
+	image2 = ImageOps.colorize(image, black, white) 
+	image2.show()'''
 
-image2 = PIL.ImageOps.expand(image, border=0, fill=0)
+if(opt == 'crop'):
+	image2 = ImageOps.crop(image, border=0)
+	image2.show()
 
-#image2 = PIL.ImageOps.fit(image, size, method=0, bleed=0.0, centering=(0.5, 0.5))
+'''if(opt == 'deform'):
+	image2 = ImageOps.deform(image, deformer, resample=2)
+	image2.show()'''
 
-image2 = PIL.ImageOps.flip(image)
-
-image2 = PIL.ImageOps.grayscale(image)
-
-image2 = PIL.ImageOps.invert(image)
-
-image2 = PIL.ImageOps.mirror(image)
-
-#image2 = PIL.ImageOps.posterize(image, bits)
-
-image2 = PIL.ImageOps.solarize(image, threshold=128)'''
-=======
-              'contour': ImageFilter.CONTOUR,
-              'detail': ImageFilter.DETAIL,
-              'edge_enhance': ImageFilter.EDGE_ENHANCE,
-              'edge_enhance_more': ImageFilter.EDGE_ENHANCE_MORE,
-              'emboss': ImageFilter.EMBOSS,
-              'find_edges': ImageFilter.FIND_EDGES,
-              'smooth': ImageFilter.SMOOTH,
-              'smooth_more': ImageFilter.SMOOTH_MORE,
-              'sharpen': ImageFilter.SHARPEN
-              }
-
-	if operation not in options:
-	   	raise ValueError("Unknown filter type.")
-
-	return image.filter(options[operation])
-
-image = Image.open(str(sys.argv[1])).convert("RGB")
-filtered = applyFilter(image, str(sys.argv[2]))
-filtered.show()
->>>>>>> e3af054bf2e928370ef4ffeba92de81811c3d519
+if(opt == 'equalize'): 
+	image2 = ImageOps.equalize(image, mask=None)
+	image2.show()
+	
+if(opt == 'expand'): 
+	image2 = ImageOps.expand(image, border=0, fill=0)
+	image2.show()
+	
+'''if(opt == 'fit'): 
+	image2 = ImageOps.fit(image, size, method=0, bleed=0.0, centering=(0.5, 0.5))
+	image2.show()'''
+	
+if(opt == 'flip'):
+	print ("testing flip") 
+	image2 = ImageOps.flip(image)
+	image2.show()
+	
+if(opt == 'grayscale'): 
+	image2 = ImageOps.grayscale(image)
+	image2.show()
+	
+if(opt == 'invert'): 
+	image2 = ImageOps.invert(image)
+	image2.show()
+	
+if(opt == 'mirror'):
+	image2 = ImageOps.mirror(image)
+	image2.show()
+	
+if(opt == 'posterize'): 
+	image2 = ImageOps.posterize(image, bits)
+	image2.show()
+	
+if(opt == 'solarize'): 
+	image2 = ImageOps.solarize(image, threshold=128)
+	image2.show()
+	
